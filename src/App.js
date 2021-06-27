0c8c6c44ac7f6ad8595489d579eb5d64c53eb906
Osamamomani1 <momaniosama97@gmail.com>
2021-06-27 16:38:12 +0300

    step2

--- src/App.js
+++ src/App.js
@@ -1,25 +1,55 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import axios from 'axios'
+import { Card,ListGroup } from 'react-bootstrap';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+export class App extends Component {
+
+  constructor(props){
+    super(props);
+    this.state={
+      displayName:'',
+      longitude:'',
+      latitude:'',
+    }
+  }
+  
+  nameHandler=(e)=>{
+    this.setState({
+      displayName: e.target.value
+    })
+  }
+
+  submitData=async (e)=>{
+    e.preventDefault()
+    let axiosData= await axios.get(`https://eu1.locationiq.com/v1/search.php?key=pk.e266821f0ad39e21a46098fffe81ff92&city=${this.state.displayName}&format=json
+    
+    `)
+    this.setState({
+      displayName: axiosData.data[0].display_name,
+      longitude: axiosData.data[0].lat,
+      latitude: axiosData.data[0].lon,
+    })
+   console.log(axiosData.data);
+  }
+  render() {
+    return (
+      <div>
+        <form onSubmit={(e)=>{this.submitData(e)}}>
+          <input type="text" placeholder="city name.." onChange={(e)=>{this.nameHandler(e)}} />
+          <button>Explore!</button>
+        </form>
+
+        <Card style={{ width: '18rem' }}>
+        <ListGroup variant="flush">
+        <ListGroup.Item>city:  <h2>{this.state.displayName}</h2></ListGroup.Item>
+        <ListGroup.Item>longitude:  <h2>{this.state.longitude}</h2></ListGroup.Item>
+        <ListGroup.Item>latitude:  <h2>{this.state.latitude}</h2></ListGroup.Item>
+        </ListGroup>
+        </Card>
+      </div>
+    )
+  }
 }
 
-export default App;
+export default App
