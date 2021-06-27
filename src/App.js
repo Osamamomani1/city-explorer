808a64153700538ba15957c09c6e62e1d234e4a2
Osamamomani1 <momaniosama97@gmail.com>
2021-06-27 18:09:02 +0300

    done3

--- src/App.js
+++ src/App.js
@@ -25,12 +25,13 @@ export class App extends Component {
     let axiosData= await axios.get(`https://eu1.locationiq.com/v1/search.php?key=pk.e266821f0ad39e21a46098fffe81ff92&city=${this.state.displayName}&format=json
     
     `)
+   
     this.setState({
       displayName: axiosData.data[0].display_name,
-      longitude: axiosData.data[0].lat,
-      latitude: axiosData.data[0].lon,
+      longitude: axiosData.data[0].lon,
+      latitude: axiosData.data[0].lat, 
     })
-   console.log(axiosData.data);
+   
   }
   render() {
     return (
@@ -45,8 +46,13 @@ export class App extends Component {
         <ListGroup.Item>city:  <h2>{this.state.displayName}</h2></ListGroup.Item>
         <ListGroup.Item>longitude:  <h2>{this.state.longitude}</h2></ListGroup.Item>
         <ListGroup.Item>latitude:  <h2>{this.state.latitude}</h2></ListGroup.Item>
+        <Card.Img variant="top" src={`https://maps.locationiq.com/v3/staticmap?key=pk.e266821f0ad39e21a46098fffe81ff92
+        &center=${this.state.latitude},${this.state.longitude}&zoom=12&format=png`} width='400px' height='300px' />
         </ListGroup>
         </Card>
+      
+        
+        
       </div>
     )
   }
